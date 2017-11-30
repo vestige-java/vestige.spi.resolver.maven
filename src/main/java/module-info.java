ed60581f8353a4262f552106c3343d7c431ef90f
Gaël Lalire <gael.lalire@laposte.net>
2017-11-30 19:43:02 +0100

    import

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module fr.gaellalire.vestige.spi.resolver.maven {
+    
+    requires transitive fr.gaellalire.vestige.spi.job;
+
+    requires transitive fr.gaellalire.vestige.spi.resolver;
+
+    exports fr.gaellalire.vestige.spi.resolver.maven;
+
+}
